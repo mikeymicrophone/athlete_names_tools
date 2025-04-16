a6c85f99f09190d118e19caf4beddb8ae4d5a3fb
Mike Schwab <mike.schwab@gmail.com>
2025-04-15 21:14:07 -0400

    city and stadium have collections

--- models/city.rb
+++ models/city.rb
@@ -9,4 +9,24 @@ class City
   def add_team(team)
     @teams << team
   end
+
+  # Returns all unique leagues of teams in this city
+  def leagues
+    @teams.map(&:league).compact.uniq
+  end
+
+  # Returns all unique games played by teams in this city
+  def games
+    @teams.flat_map { |team| team.respond_to?(:games) ? team.games : [] }
+  end
+
+  # Returns all players in all teams in this city
+  def players
+    @teams.flat_map { |team| team.players }
+  end
+
+  # Returns all unique sports of teams in this city
+  def sports
+    @teams.map { |team| team.sport }.compact.uniq
+  end
 end
--- models/stadium.rb
+++ models/stadium.rb
@@ -10,4 +10,24 @@ class Stadium
   def add_team(team)
     @teams << team
   end
+
+  # Returns all unique leagues of teams in this stadium
+  def leagues
+    @teams.map(&:league).compact.uniq
+  end
+
+  # Returns all unique games played in this stadium by its teams
+  def games
+    @teams.flat_map { |team| team.respond_to?(:games) ? team.games : [] }
+  end
+
+  # Returns all players in all teams in this stadium
+  def players
+    @teams.flat_map { |team| team.players }
+  end
+
+  # Returns all unique sports of teams in this stadium
+  def sports
+    @teams.map { |team| team.sport }.compact.uniq
+  end
 end
