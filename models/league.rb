1da5b7d24aadcfee9e21cce93e108151e250a689
Mike Schwab <mike.schwab@gmail.com>
2025-04-15 21:51:20 -0400

    seed file for NBA playoffs

--- models/league.rb
+++ models/league.rb
@@ -1,10 +1,12 @@
 class League
   attr_accessor :name, :teams, :sport
+  @@all_leagues = []
 
   def initialize(name, sport)
     @name = name
     @sport = sport
     @teams = []
+    @@all_leagues << self
   end
 
   def add_team(team)
@@ -29,5 +31,13 @@ class League
   # Returns all games played by teams in this league
   def games
     @teams.flat_map { |team| team.respond_to?(:games) ? team.games : [] }
+  end
+
+  def self.find_by_name(name)
+    @@all_leagues.find { |league| league.name == name }
+  end
+
+  def self.all
+    @@all_leagues
   end
 end
