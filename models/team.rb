092986d3dfcf599f2fabc0a928ab70d318c90dcb
Mike Schwab <mike.schwab@gmail.com>
2025-04-15 21:40:26 -0400

    begin making finder methods for use in ingest

--- models/team.rb
+++ models/team.rb
@@ -1,5 +1,6 @@
 class Team
   attr_accessor :name, :players, :league, :city, :stadium
+  @@all_teams = []
 
   def initialize(name, league, city = nil, stadium = nil)
     @name = name
@@ -7,6 +8,7 @@ class Team
     @city = city
     @stadium = stadium
     @players = []
+    @@all_teams << self
   end
 
   def add_player(player)
@@ -15,5 +17,13 @@ class Team
 
   def sport
     @league.sport if @league
+  end
+
+  def self.find_by_name(name)
+    @@all_teams.find { |team| team.name == name }
+  end
+
+  def self.all
+    @@all_teams
   end
 end
